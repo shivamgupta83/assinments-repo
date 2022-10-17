399522b454941a1b694815751e2a5e4c0b13614c
shivamgupta83 <shivam8359083814@gmail.com>
2022-10-17 18:51:12 +0530

    assinment2-17-10-2022

--- src/routes/route.js
+++ src/routes/route.js
@@ -31,6 +31,91 @@ router.get('/students/:studentName', function(req, res){
     res.send('The full name is ' + myParams.studentName )
 })
 
+// 1st assinment
+router.get('/movies', function (req, res){
+  // console.log("The path params in the request are : ", req.params)
+
+  let movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+
+  res.send(movies)
+})
+
+//2nd assinment
+// router.get('/movies/:indexNumber', function (req, res){
+//   // console.log("The path params in the request are : ", req.params)
+// let movies2 = req.params
+//   let movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+
+//   res.send(movies[movies2.indexNumber])
+// })
+
+//2 and 3rd assinment
+
+router.get('/movies/:indexNumber', function (req, res){
+  // console.log("The path params in the request are : ", req.params)
+let movies2 = req.params
+  let movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+if (movies2.indexNumber<=movies.index){
+  res.send(movies[movies2.indexNumber])
+}
+   else {
+    res.send("not found !")
+   }
+})
+
+//4th  assinment 
+
+router.get('/films', function (req, res){
+  //   // console.log("The path params in the request are : ", req.params)
+     
+     let movies = [ {
+      "id": 1,
+      "name": "The Shining"
+     }, {
+      "id": 2,
+      "name": "Incendies"
+     }, {
+      "id": 3,
+      "name": "Rang de Basanti"
+     }, {
+      "id": 4,
+      "name": "Finding Nemo"
+     }]
+     
+  
+      res.send(movies)
+
+    })
+
+
+    router.get('/films/:filmId', function (req, res){
+      //   // console.log("The path params in the request are : ", req.params)
+      let no=req.params   
+
+         let movies = [ {
+          "id": 1,
+          "name": "The Shining"
+         }, {
+          "id": 2,
+          "name": "Incendies"
+         }, {
+          "id": 3,
+          "name": "Rang de Basanti"
+         }, {
+          "id": 4,
+          "name": "Finding Nemo"
+         }]
+
+         if (no.filmId<=3){
+                res.send(movies[no.filmId])
+         }
+        else {
+             res.send("not found!")
+            }
+        })
+    
+
+
 // Example 2 for path params
 router.get('/student-details/:name', function(req, res){
     let requestParams = req.params
